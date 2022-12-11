d1ad86ff76674bf5205b4128ff4cba96fb1534f5
Hayden Chen <brooklyn1984@qq.com>
2022-12-12 07:58:00 +0800

    feat: ✨ Supports echarts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import '@logseq/libs'
 import { LSPluginBaseInfo, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin'
 import mermaidRenderer from './mermaid'
+import echartsRenderer from './echarts'
 
 const Supports = {
-  Mermaid: 'mermaid'
+  Mermaid: 'mermaid',
+  Echarts: 'echarts',
 }
 
 const settingsSchema: Array<SettingSchemaDesc> = [
@@ -13,7 +15,14 @@ const settingsSchema: Array<SettingSchemaDesc> = [
     title: 'Support mermaid?',
     description: 'Mermaid lets you create diagrams and visualizations using text and code.',
     default: true
-  }
+  },
+  {
+    key: 'echarts',
+    type: 'boolean',
+    title: 'Support echarts?',
+    description: 'Use Echarts to render the chart.',
+    default: true
+  },
 ]
 
 function main (baseInfo: LSPluginBaseInfo) {
@@ -24,6 +33,14 @@ function main (baseInfo: LSPluginBaseInfo) {
       Supports.Mermaid, {
         edit: true,
         render: mermaidRenderer
+      }
+    )
+  }
+  if (settings.echarts) {
+    logseq.Experiments.registerFencedCodeRenderer(
+      Supports.Echarts, {
+        edit: true,
+        render: echartsRenderer,
       }
     )
   }
